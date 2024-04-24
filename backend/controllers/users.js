981887d9677bfa718316bf4b857ed017c9ad4cfc
Valeri Karpov <val@karpov.io>
2024-04-24 09:48:57 -0400

    make cookie not HTTP only

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -60,7 +60,7 @@ const signup = async (req, res, next) => {
     // Set cookie in the browser to store authentication state
     const maxAge = 1000 * 60 * 60 * 24 * 3; // 3 days
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       maxAge: maxAge,
       domain: process.env.DOMAIN,
       sameSite: "None",
