0e5d5339f81f93cc43621c88bc498e7be643fdd7
Valeri Karpov <val@karpov.io>
2024-07-17 14:36:26 -0400

    copy mongoose studio files to top-level

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,7 @@ const withImages = require("next-images");
 const { execSync } = require("child_process");
 require("@mongoosejs/studio/frontend")('/api/studio', true).then(() => {
   execSync(`
-  mkdir -p ./public/studio
-  cp -r ./node_modules/@mongoosejs/studio/frontend/public/* ./public/studio/
+  cp -r ./node_modules/@mongoosejs/studio/frontend/public/* ./public/
   `);
 });
 
