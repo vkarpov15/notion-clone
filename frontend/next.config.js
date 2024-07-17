afeea2564c1fad4f7cc61ea014c7ad0a6ac140c1
Valeri Karpov <val@karpov.io>
2024-07-17 14:41:04 -0400

    copy studio to different location for nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const withImages = require("next-images");
 const { execSync } = require("child_process");
 require("@mongoosejs/studio/frontend")('/api/studio', true).then(() => {
   execSync(`
+  mkdir -p ./public/studio
+  cp -r ./node_modules/@mongoosejs/studio/frontend/public/* ./public/studio/
   cp -r ./node_modules/@mongoosejs/studio/frontend/public/* ./public/
   `);
 });
